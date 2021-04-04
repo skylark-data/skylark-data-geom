b1efb437332126d56ebcb22f9b05b6a2750346e1
hudaokeji <github@hudaokeji.com>
2021-04-04 16:21:38 +0900

    update

--- dist/skylark-graphics-shapes/transform/SkewTransform.js
+++
@@ -1,9 +0,0 @@
-/**
- * skylark-graphics-shapes - The skylark shape class library.
- * @author Hudaokeji Co.,Ltd
- * @version v0.9.1
- * @link www.skylarkjs.org
- * @license MIT
- */
-define(["skylark-langx/langx","../shapes","./Transform","./Matrix","../Point","../Rect"],function(n,t,e,s,r){return shapes.SkewTransform=t.inherit({klassName:"SkewTransform",value:{get:function(){return e.scaleAt(this.skewX,this.skewY)}},skewX:{get:function(){return this._.skewX}},skewY:{get:function(){return this._.skewY}},clone:function(){},transform:function(n){},transformBounds:function(n){},init:function(n,t){var e=this._;e.skewX=n||0,e.skewY=t||0}})});
-//# sourceMappingURL=../sourcemaps/transform/SkewTransform.js.map
--- dist/skylark-graphics-shapes/transform/Transform.js
+++
@@ -1,9 +0,0 @@
-/**
- * skylark-graphics-shapes - The skylark shape class library.
- * @author Hudaokeji Co.,Ltd
- * @version v0.9.1
- * @link www.skylarkjs.org
- * @license MIT
- */
-define(["skylark-langx/langx","../shapes","./Matrix"],function(a,n,r){return n.Transform=a.klass({klassName:"Transform",value:{get:function(){return this._.value}}})});
-//# sourceMappingURL=../sourcemaps/transform/Transform.js.map
